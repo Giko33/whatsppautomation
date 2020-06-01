a92f20b90bb519472e641e54d735c82dcf18bfcc
Sayan Datta <sayandatta33@gmail.com>
2020-06-01 22:22:21 +0530

    initial

--- venv/auto.py
+++ venv/auto.py
@@ -1,10 +1,10 @@
 from selenium import webdriver
 import time
-browser=webdriver.Edge(executable_path='C:\Setups\msedgedriver')
-browser.get("https://web.whatsapp.com/")
+browser=webdriver.Edge(executable_path='C:\Setups\msedgedriver') #path where the edge driver in my system is saved
+browser.get("https://web.whatsapp.com/")                         #used "Edge" for microsoft edge browser will change accordingly if any other browser is used
 time.sleep(10)
 
-sender=input("Enter the recipent")
+sender=input("Enter the recipent")                               #name of the recipent
 find=browser.find_element_by_xpath('//div[@title="New chat"]')
 find.click()
 find1=browser.find_element_by_xpath('//div[@dir="ltr"]')
@@ -16,8 +16,8 @@ find1.send_keys(sender)
 send=browser.find_element_by_xpath('//span[@title="{}"]'.format(sender))
 send.click()
 
-n=int(input("Enter the number of times you want to send the message"))
-message=input("Enter the message")
+n=int(input("Enter the number of times you want to send the message"))  #the count of the text
+message=input("Enter the message")                                      #the message
 
 for i in range(n):
     box=browser.find_element_by_xpath('//div[@class="_1Plpp"]')
